0851e1556a9e2f08ce8cb3276bacb584be75fbef
sus228000 <pewpewshoot.sus228000@gmail.com>
2026-07-23 20:12:48 +0000

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,1 @@
-.\srcds.exe -game csgo -tickrate 128 -debug +sv_setsteamaccount YOURTOKENHERE -port 27015 -console -usercon +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2
+.\srcds.exe -game csgo -tickrate 128 -debug +sv_setsteamaccount YOURTOKENHERE -port 27015 -console -usercon +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2 +sv_use_steam_networking 1
